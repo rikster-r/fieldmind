b0051251cf516068fda14b1b0a91552f80c5057d
rikster-r <seitdanial43@gmail.com>
2024-11-13 21:13:12 +0500

    Add max width to sections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,47 +7,48 @@ export default function About() {
       id="about"
       className="bg-secondary-200 px-4 py-10 text-primary-100 md:pb-20 lg:px-32"
     >
-      <h2 className="outline-title outline-rise text-5xl font-black uppercase tracking-wide md:mt-10 md:text-left md:text-8xl">
-        Our Team
-      </h2>
-      <div className="flex h-full flex-col md:flex-row md:justify-between">
-        <div className="mx-1 mt-4 flex min-h-full flex-col gap-3 leading-5 md:mt-6 md:gap-8 md:text-3xl">
-          <div>
-            <p>Orazbay Nurkhan</p>
-            <p>- developer, programmer</p>
-          </div>
-          <div>
-            <p>Bukhanova Elmira</p>
-            <p>- UI/UX designer</p>
-          </div>
-          <div>
-            <p>Akanov Farhad</p>
-            <p>- manager and analyst</p>
-          </div>
-          <div className="mt-6 md:mt-auto">
-            <p>Zarina Almabekovna </p>
-            <p>- Head, PhD in Drones</p>
-          </div>
-        </div>
-
-        <div className="flex flex-col md:w-1/2">
-          <div className="relative z-0 ml-auto mt-4 aspect-square w-full max-w-[500px] md:mt-0">
-            <div className="absolute left-0 top-0 z-10 aspect-square w-[85%]">
-              <Image
-                src={Team}
-                alt="Team image"
-                layout="fill"
-                className="h-full w-full"
-              />
+      <div className="max-w-[1300px] mx-auto">
+        <h2 className="outline-title outline-rise text-5xl font-black uppercase tracking-wide md:mt-10 md:text-left md:text-8xl">
+          Our Team
+        </h2>
+        <div className="flex flex-col md:flex-row md:justify-between">
+          <div className="mx-1 mt-4 flex min-h-full flex-col gap-3 leading-5 md:mt-6 md:gap-8 md:text-3xl">
+            <div>
+              <p>Orazbay Nurkhan</p>
+              <p>- developer, programmer</p>
             </div>
-            <div
-              className="absolute bottom-0 right-0 z-0 aspect-square w-1/2"
-              id="x-fill"
-            ></div>
+            <div>
+              <p>Bukhanova Elmira</p>
+              <p>- UI/UX designer</p>
+            </div>
+            <div>
+              <p>Akanov Farhad</p>
+              <p>- manager and analyst</p>
+            </div>
+            <div className="mt-6 md:mt-auto">
+              <p>Zarina Almabekovna </p>
+              <p>- Head, PhD in Drones</p>
+            </div>
           </div>
-          <button className="ml-auto mt-4 w-max border-[3px] border-primary-100 px-4 py-2 text-center text-sm font-extrabold uppercase tracking-wide text-primary-100 hover:opacity-80 focus:outline-none focus:ring-4 focus:ring-primary-300 sm:text-lg md:mt-12 md:border-4 md:px-5 md:py-3">
-            View portfolio
-          </button>
+          <div className="flex flex-col md:w-1/2">
+            <div className="relative z-0 ml-auto mt-4 aspect-square w-full max-w-[500px] md:mt-0">
+              <div className="absolute left-0 top-0 z-10 aspect-square w-[85%]">
+                <Image
+                  src={Team}
+                  alt="Team image"
+                  layout="fill"
+                  className="h-full w-full"
+                />
+              </div>
+              <div
+                className="absolute bottom-0 right-0 z-0 aspect-square w-1/2"
+                id="x-fill"
+              ></div>
+            </div>
+            <button className="ml-auto mt-4 w-max border-[3px] border-primary-100 px-4 py-2 text-center text-sm font-extrabold uppercase tracking-wide text-primary-100 hover:opacity-80 focus:outline-none focus:ring-4 focus:ring-primary-300 sm:text-lg md:mt-12 md:border-4 md:px-5 md:py-3">
+              View portfolio
+            </button>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,31 +7,31 @@ export default function Hero() {
       className="bg-gradient-to-br from-tertiary-100 to-secondary-200 px-4 py-10 text-primary-100 md:pb-20 lg:px-32"
       id="home"
     >
-      <p className="text-[8px] uppercase tracking-wider md:mt-10 md:text-xl md:tracking-widest">
-        drons | ground equipment | Ai
-      </p>
-      <h1
-        className="outline-title outline-rise mt-2 text-3xl font-black uppercase tracking-wide md:text-8xl"
-      >
-        FIELDMINDE
-      </h1>
-      <div className="relative z-0 ml-auto mt-10 h-[100px] w-[250px] md:h-[250px] md:w-[650px]">
-        <Image
-          src={Drone}
-          alt="Drone image"
-          layout="fill"
-          className="h-full w-full"
-        />
-      </div>
-      <div className="mt-10 flex flex-col gap-4 md:flex-row md:items-center md:justify-between lg:mt-20">
-        <p className="max-w-[90%] text-2xl md:max-w-[70%] md:text-4xl">
-          We create a universal project and expand its potential for various
-          applications, allowing the user to adapt the system according to
-          specific needs.
+      <div className="max-w-[1300px] mx-auto">
+        <p className="text-[8px] uppercase tracking-wider md:mt-10 md:text-xl md:tracking-widest">
+          drons | ground equipment | Ai
         </p>
-        <button className="w-max bg-primary-100 px-7 py-3 text-center text-sm font-bold uppercase tracking-wide text-secondary-200 hover:opacity-80 focus:outline-none focus:ring-4 focus:ring-primary-300 sm:text-lg md:px-9 md:py-4">
-          Get in touch
-        </button>
+        <h1 className="outline-title outline-rise mt-2 text-3xl font-black uppercase tracking-wide md:text-8xl">
+          FIELDMINDE
+        </h1>
+        <div className="relative z-0 ml-auto mt-10 h-[100px] w-[250px] md:h-[250px] md:w-[650px]">
+          <Image
+            src={Drone}
+            alt="Drone image"
+            layout="fill"
+            className="h-full w-full"
+          />
+        </div>
+        <div className="mt-10 flex flex-col gap-4 md:flex-row md:items-center md:justify-between lg:mt-20">
+          <p className="max-w-[90%] text-2xl md:max-w-[70%] md:text-4xl">
+            We create a universal project and expand its potential for various
+            applications, allowing the user to adapt the system according to
+            specific needs.
+          </p>
+          <button className="w-max bg-primary-100 px-7 py-3 text-center text-sm font-bold uppercase tracking-wide text-secondary-200 hover:opacity-80 focus:outline-none focus:ring-4 focus:ring-primary-300 sm:text-lg md:px-9 md:py-4">
+            Get in touch
+          </button>
+        </div>
       </div>
     </section>
   );
